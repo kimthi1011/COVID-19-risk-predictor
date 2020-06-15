6724272f20c9600739b353836055f2405da0fd4d
Tarun karthik kumar Mamidi <tmamidi@login002.cm.cluster>
2020-06-15 14:02:49 -0500

    change in measurement code - count number of tests

--- filter_dataset.py
+++ filter_dataset.py
@@ -5,14 +5,16 @@ import re
 test = pd.read_csv('../UAB_LDS/Cohort1/2020-JUNE-hackathon-N3C-covid19-Positive/measurement.csv', sep="|", header = 0)
 test.head()
 test = test[['PERSON_ID','MEASUREMENT_DATE','MEASUREMENT_SOURCE_VALUE','VALUE_SOURCE_VALUE']]
+test=test[test.PERSON_ID.isin(pid)]
+len(test['MEASUREMENT_SOURCE_VALUE'])
 test = test[test.MEASUREMENT_SOURCE_VALUE.str.contains('cov',regex=True,na=False, flags = re.IGNORECASE) & ~test.MEASUREMENT_SOURCE_VALUE.str.contains('quest',regex=True,na=False, flags = re.IGNORECASE)]
 pid = test.PERSON_ID.unique().tolist()
 test['MEASUREMENT_DATE'] = pd.to_datetime(test['MEASUREMENT_DATE'])
 test.to_csv('./parsed_data/tested.csv', index=False)
 
 #Find number of patients who got tested
-test1=pd.read_csv('./parsed_data/condition_data_positive.csv')
-pid = test.PERSON_ID.unique().tolist()
+test1=pd.read_csv('./parsed_data/person_positive.csv')
+pid = test1.PERSON_ID.unique().tolist()
 #len(pid)
 
 
@@ -39,13 +41,13 @@ df.to_csv('./parsed_data/concepts.csv', index=False)
 
 
 #open and read observation.csv file
-test2 = pd.read_csv('observation.csv', sep="|", header = 0)
+test2 = pd.read_csv('../UAB_LDS/Cohort3/2020-JUNE-hackathon-N3C-covid19-Negative/observation.csv', sep="|", header = 0)
 test2 = test2[['PERSON_ID','OBSERVATION_DATE','VALUE_AS_STRING','OBSERVATION_SOURCE_VALUE','QUALIFIER_SOURCE_VALUE']]
 len(test2.index)
 test2=test2[test2.PERSON_ID.isin(pid)]
 test2['OBSERVATION_DATE'] = pd.to_datetime(test2['OBSERVATION_DATE'])
 #test1.dtypes
-test2.to_csv('./parsed_data/observation.csv',index=False)
+test2.to_csv('./parsed_data/observation_Negative.csv',index=False)
 
 
 #open and read person.csv file
